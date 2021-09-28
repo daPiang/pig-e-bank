c95bbcbcb2978319d359a52f70f994421df4b000
Whytel Macro <daPiang@mcm.edu.ph>
2021-09-28 21:39:09 +0800

    cleaned main.dart; added login.dart(WIP)

---
+++ lib/login.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final userController = TextEditingController();
+  final passController = TextEditingController();
+  final snackBar = SnackBar(content: Text('Invalid Login'));
+  bool _isAdmin = false;
+
+  @override
+  void dispose() {
+    userController.dispose();
+    passController.dispose();
+    super.dispose();
+  }
+
+  void authLogin() {
+    if (userController.text == "admin" && passController.text == "password") {
+      print('Welcome back, admin.');
+      _isAdmin = true;
+    } else {
+      print('Unrecognized credentials');
+      _isAdmin = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: userController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Username',
+                fillColor: Colors.pink[100],
+              ),
+            ),
+            TextField(
+              controller: passController,
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                fillColor: Colors.pink[100],
+              ),
+            ),
+            ElevatedButton(
+                child: Text('Login'),
+                onPressed: () {
+                  authLogin();
+                  if (_isAdmin == false) {
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
